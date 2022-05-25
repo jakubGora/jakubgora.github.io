d2d115babe079fd0fce655adc3b55402a4e6d73c
jakubGora <jakub_gora@wp.pl>
2022-05-25 17:19:33 +0200

    Portfolio

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -7,14 +7,42 @@ import {
 import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-
+import SimpleImageSlider from "react-simple-image-slider";
 function Portfolio() {
   const canie = require("../../img/canien.png");
   const memo = require("../../img/memory.png");
   const play = require("../../img/playapp.png");
-  const unsplash = require("../../img/unsplashapp.png");
-  const tie = require("../../img/tieapp.png");
 
+  const finowLogo = require("../../img/logo_finow.png");
+  const accessLogo = require("../../img/Microsoft-Access-Logo.png");
+  const finow = [
+    { url: require("../../img/finow/Finow1.png") },
+    { url: require("../../img/finow/Finow2.png") },
+    { url: require("../../img/finow/Finow3.png") },
+    { url: require("../../img/finow/Finow4.png") },
+  ];
+  const tie = [
+    { url: require("../../img/tieApp/1.png") },
+    { url: require("../../img/tieApp/2.png") },
+    { url: require("../../img/tieApp/3.png") },
+    { url: require("../../img/tieApp/4.png") },
+    { url: require("../../img/tieApp/5.png") },
+    { url: require("../../img/tieApp/6.png") },
+  ];
+  const takeCare = [
+    { url: require("../../img/takeCare/1.png") },
+    { url: require("../../img/takeCare/2.png") },
+    { url: require("../../img/takeCare/3.png") },
+  ];
+  const spaceman = [
+    { url: require("../../img/SpaceMan/1.png") },
+    { url: require("../../img/SpaceMan/2.png") },
+    { url: require("../../img/SpaceMan/3.png") },
+  ];
+  const unsplash = [
+    { url: require("../../img/unSplash/unsplashapp.png") },
+    { url: require("../../img/unSplash/onSplash2.png") },
+  ];
   return (
     <div className="Portfolio">
       <div className="title">
@@ -22,6 +50,44 @@ function Portfolio() {
       </div>
 
       <div className="gallery">
+        <div className="project">
+          <div className="descr">
+            {" "}
+            <div>
+              {" "}
+              <h2>
+                Narzędziownia dla firmy <br />
+                FINOW POLSKA Sp. z o.o.
+              </h2>
+              <p>
+                Aplikacja przeznaczona do <br /> zarządzania oraz obsługi
+                narzędziownii.
+              </p>
+            </div>
+            <br />
+            {/* <img width={"100px"} height="100px" src={finowLogo} alt="" /> */}
+            <div className="tech">
+              <img width={"140px"} src={accessLogo} />
+            </div>
+          </div>
+
+          <SimpleImageSlider
+            width={"100%"}
+            height={1000}
+            autoPlay={true}
+            style={{
+              position: "relative",
+              width: "100%",
+              color: "black",
+
+              objectFit: "cover",
+            }}
+            images={finow}
+            showBullets={true}
+            showNavs={true}
+            navStyle={2}
+          />
+        </div>
         <div className="project">
           <div className="descr">
             {" "}
@@ -63,8 +129,24 @@ function Portfolio() {
             </div>
           </div>
 
-          <img className="object" src={unsplash}></img>
+          <SimpleImageSlider
+            width={"100%"}
+            height={1000}
+            autoPlay={true}
+            style={{
+              position: "relative",
+              width: "100%",
+              color: "black",
+              objectFit: "cover",
+              objectPosition: "center bottom",
+            }}
+            images={unsplash}
+            showBullets={true}
+            showNavs={true}
+            navStyle={2}
+          />
         </div>
+
         <div style={{ width: "50%" }} className="project">
           <div className="descr">
             {" "}
@@ -102,7 +184,22 @@ function Portfolio() {
               ></FontAwesomeIcon>
             </div>
           </div>
-          <img className="object" src={tie}></img>
+          <SimpleImageSlider
+            width={"100%"}
+            height={1000}
+            autoPlay={true}
+            style={{
+              position: "relative",
+              width: "100%",
+              color: "black",
+              objectFit: "cover",
+              objectPosition: "center bottom",
+            }}
+            images={tie}
+            showBullets={true}
+            showNavs={true}
+            navStyle={2}
+          />
         </div>
         <div style={{ width: "50%" }} className="project">
           <div className="descr">
@@ -220,6 +317,116 @@ function Portfolio() {
             </div>
           </div>
           <img className="object" src={canie}></img>
+        </div>
+        <div style={{ width: "50%" }} className="project">
+          <div className="descr">
+            {" "}
+            <div>
+              {" "}
+              <h2>Take Care android game</h2>
+              <p>Prosta gra ze skaczącą kulką.</p>
+            </div>{" "}
+            <div className="tech">
+              <img
+                className="unity"
+                src="https://img.icons8.com/ios-filled/100/000000/unity.png"
+              />
+              <img src="https://img.icons8.com/color/120/000000/c-sharp-logo-2.png" />
+              <img src="https://img.icons8.com/fluency/120/000000/android-os.png" />
+            </div>{" "}
+            <div
+              onClick={() =>
+                window.open(
+                  "https://play.google.com/store/apps/details?id=com.GoraSoft.TakeCare&hl=pl&gl=US",
+                  "_blank"
+                )
+              }
+              className="demo"
+            >
+              <p>
+                {" "}
+                <br />
+                Live demo
+                <br /> here{" "}
+              </p>
+
+              <FontAwesomeIcon
+                icon={faPaperclip}
+                fontSize="3rem"
+              ></FontAwesomeIcon>
+            </div>
+          </div>
+          <SimpleImageSlider
+            width={"100%"}
+            height={1000}
+            autoPlay={true}
+            style={{
+              position: "relative",
+              width: "100%",
+              color: "black",
+              objectFit: "cover",
+              objectPosition: "center bottom",
+            }}
+            images={takeCare}
+            showBullets={true}
+            showNavs={true}
+            navStyle={2}
+          />
+        </div>
+        <div style={{ width: "50%" }} className="project">
+          <div className="descr">
+            {" "}
+            <div>
+              {" "}
+              <h2>Spaceman - kosmiczna podróż</h2>
+              <p>Prosta gra o kosmosie.</p>
+            </div>{" "}
+            <div className="tech">
+              <img
+                className="unity"
+                src="https://img.icons8.com/ios-filled/100/000000/unity.png"
+              />
+              <img src="https://img.icons8.com/color/120/000000/c-sharp-logo-2.png" />
+              <img src="https://img.icons8.com/fluency/120/000000/android-os.png" />
+            </div>{" "}
+            <div
+              onClick={() =>
+                window.open(
+                  "https://play.google.com/store/apps/details?id=com.Gravity.Space.Symulator&hl=pl&gl=US",
+                  "_blank"
+                )
+              }
+              className="demo"
+            >
+              <p>
+                {" "}
+                <br />
+                Live demo
+                <br /> here{" "}
+              </p>
+
+              <FontAwesomeIcon
+                icon={faPaperclip}
+                fontSize="3rem"
+              ></FontAwesomeIcon>
+            </div>
+          </div>
+          <SimpleImageSlider
+            width={"100%"}
+            height={1000}
+            autoPlay={true}
+            style={{
+              position: "relative",
+              width: "100%",
+              color: "black",
+              objectFit: "cover",
+              objectPosition: "center bottom",
+            }}
+            images={spaceman}
+            showBullets={true}
+            showNavs={true}
+            navStyle={2}
+          />
         </div>
       </div>
       <a href="https://icons8.com/icon/uJM6fQYqDaZK/typescript">
